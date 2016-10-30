c23fd6037cee376042f6f492c73576e222742e6a
allen <allen>
2016-10-30 09:24:22 +0800

    support markdown

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 class Post(models.Model):
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
-    date_time = models.DateTimeField(auto_now_add = True)  #博客日期
+    date_time = models.DateTimeField('发布时间', default=timezone.now)
+
 
     class Meta:  #按时间下降排序
         ordering = ['-date_time']
